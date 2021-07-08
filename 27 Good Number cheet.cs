5b1ddd16f5e4383a2a1903131e76b4f51fd05d59
FirstOneMohamedAhmedAbdAlaziz <mohamed.ahmed799999@gmail.com>
2021-07-08 19:48:44 +0200

    seach Algorithm

---
+++ 27 Good Number cheet.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] st = Console.ReadLine().Split();
+            int n = Convert.ToInt32( st[0]);
+            int k= Convert.ToInt32( st[1]);
+            int cout = 0;
+            for (int i = 0; i<n;i++ )
+            {
+                string t =Console.ReadLine();
+                char[] t2 = t.ToCharArray();
+                int co = 0;
+                for (int j = 0; j <= k; j++)
+                {
+                    
+                    for (int b = 0; b < t2.Length; b++)
+                    {
+                        if (j == Convert.ToInt32(t2[b].ToString())){
+                            co++;
+                            break;
+                        }
+                    }
+                  
+                }
+                if (co == k + 1)
+                {
+                    cout++;
+                }
+                
+
+
+            }
+
+            Console.WriteLine(cout);
+        }
+    }
+}
